dcec85fbc98c9aee05b63fb4fd86dd9f24558959
Muhammad Ammar Afif <amarafif230@gmail.com>
2023-11-01 17:06:40 +0700

    fix: auth

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express'),
     router = express.Router(),
-    authRouter = require('./auth'),
-    categoryRouter = require('./category'),
-    itemRouter = require('./item')
+    authRouter = require('./auth')
+    // categoryRouter = require('./category'),
+    // itemRouter = require('./item')
 
 router.use('/auth', authRouter)
-router.use('/category', categoryRouter)
-router.use('/item', itemRouter)
+// router.use('/category', categoryRouter)
+// router.use('/item', itemRouter)
 
 module.exports = router
